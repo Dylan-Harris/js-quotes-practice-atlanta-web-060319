bc319fdd07a3891bcab3383924f2705b0b8cec99
Dylan Harris <dylanharris23@gmail.com>
2019-07-22 15:06:25 -0400

    On to the buttons

--- src/index.js
+++ src/index.js
@@ -1,2 +1,56 @@
 // It might be a good idea to add event listener to make sure this file 
 // only runs after the DOM has finshed loading. 
+document.addEventListener("DOMContentLoaded", setUpPage)
+
+function setUpPage() {
+    let form = document.querySelector('#new-quote-form')
+    form.addEventListener('submit', handleSubmit)
+
+    getAllQuotes()
+}
+
+function handleSubmit(e) {
+    e.preventDefault()
+
+    let info = {
+        quote: e.target.quote.value,
+        author: e.target.author.value,
+        likes : 0
+    }
+}
+
+function addNewQuote() {
+    e.target.reset
+}
+
+function getAllQuotes() {
+    fetch('http://localhost:3000/quotes?_embed=likes')
+    .then(res => res.json())
+    .then(data => createQuoteList(data))
+}
+
+function createQuoteList(quotes) {
+    quotes.forEach(quoteCards)
+}
+
+function quoteCards(info) {
+const ul = document.querySelector('ul')
+
+let li = document.createElement('li')
+li.className = "quote-card"
+
+let blockBox = document.createElement("blockquote")
+blockBox.className = "blockquote"
+
+let quote = document.createElement('p')
+quote.innerText = info.quote
+
+let footer = document.createElement('footer')
+footer.className = 'blockquote-footer'
+footer.innerText = info.author
+
+ul.appendChild(li)
+li.appendChild(blockBox)
+li.appendChild(quote)
+quote.appendChild(footer)
+}
